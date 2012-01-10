219b2124b76407261a481e821b902a44d3f11c91
Michael Melia <mikemelia@gmail.com>
2012-01-11 07:40:11 +1100

    still fixing it up

--- Gusfield/c-solutions/pre-processor/z-algorithm.c
+++ Gusfield/c-solutions/pre-processor/z-algorithm.c
@@ -8,7 +8,6 @@ struct list {
 };
 
 int called = 0;
-char *string = "ACGCCGACGCAGGAACTACGCCGACGCAGGAACTAC";
 
 int bruteForceZValue(char *sub, char *reference, int length) {
 	int counter = 0;
@@ -23,42 +22,46 @@ int bruteForceZValue(char *sub, char *reference, int length) {
 	return counter;
 }
 
+int getValueFor(int k, char *reference, int length, int *l, int *r) {
+	int value = bruteForceZValue((reference + k), reference, length - k);
+	if (value > 0) {
+		*r = k - 1 + value;
+		*l = k - 1;
+	}
+	return value;
+}
+
 void zValuesFor(char *reference, int zvalues[], int length) {
 	int k;
 	int r = 0; 
 	int l = 0;
 	zvalues[0] = 0;
-	zvalues[1] = bruteForceZValue((reference + 1), reference, length - 1);
-	if (zvalues[1] > 0) {
-		r = zvalues[1];
-		l = 1;
-	}
+	zvalues[1] = getValueFor(1, reference, length, &l, &r);
 	for (k = 2; k < length; k++) {
 		if (k >= r) {
-			printf("r = %d", r);
-			zvalues[k] = bruteForceZValue((reference + k), reference, length - k);
+			zvalues[k] = getValueFor(k, reference, length, &l, &r);
 		} else {
 			int kprime = k - l;
-			if (zvalues[kprime] < (r - k)) {
+			if (zvalues[kprime] < (r - k - 1)) {
 				zvalues[k] = zvalues[kprime];
 			} else {
 				int q = bruteForceZValue((reference + r), reference + zvalues[kprime], length - r);
-				zvalues[k] = r + q - k;
+				zvalues[k] = r + q - k - 1;
 				r = r + q;
 				l = k;
 			}
-			printf("k= %d, r = %d", k, r);
 		}
 	}
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 	int *zvalues;
+	char *string = argv[1];
 	int length = strlen(string);
 	int counter;
 	zvalues = malloc(sizeof(int) * length);
 	zValuesFor(string, zvalues, length);
-    printf("Called %d times.\n", called);
+    printf("Called %d times for a length of %d %s.\n", called, length, string);
 	for (counter = 0; counter < length; counter++) {
 	    printf("%d %d\n", counter, *(zvalues + counter));
 	}
